abc024e200e222b387cbb19d2ada6b0974c90f61
mhd-zaid <zaidmouhamad@gmail.com>
2023-06-12 16:35:54 +0200

    user-customer part1

--- src/DataFixtures/CustomerFixtures.php
+++ src/DataFixtures/CustomerFixtures.php
@@ -2,7 +2,7 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\User;
+use App\Entity\Customer;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -15,57 +15,33 @@ class UserFixtures extends Fixture
         $faker = Factory::create('fr_FR');
         $pwd = '$2y$13$cxyypcYcyj4sQhaeLhojvucbBwbWo789iF/Aqqsvm2Rpcu/jNxIf6';
 
-        $object = (new User())
+        $object = (new Customer())
             ->setEmail('customer@user.fr')
             ->setLastname($faker->lastName)
             ->setFirstname($faker->firstName)
             ->setRoles([])
             ->setPassword($pwd)
+            ->setAddress($faker->address)
+            ->setPhone($faker->phoneNumber)
             ->setValidationToken("")
             ->setCreatedAt(new DateTime())
             ->setUpdatedAt(new DateTime())
         ;
         $manager->persist($object);
 
-        $object = (new User())
-            ->setEmail('accountant@user.fr')
+        $object = (new Customer())
+            ->setEmail('customer2@user.fr')
             ->setLastname($faker->lastName)
             ->setFirstname($faker->firstName)
-            ->setRoles(['ROLE_ACCOUNTANT'])
+            ->setRoles([])
             ->setPassword($pwd)
-            ->setValidationToken("")
-            ->setCreatedAt(new DateTime())
-            ->setUpdatedAt(new DateTime())
-        ;
-        $manager->persist($object);
-
-        $object = (new User())
-        ->setEmail('mechanic@user.fr')
-        ->setLastname($faker->lastName)
-        ->setFirstname($faker->firstName)
-        ->setRoles(['ROLE_MECHANIC'])
-        ->setPassword($pwd)
-        ->setIsValidated(true)
-        ->setValidationToken("")
-        ->setCreatedAt(new DateTime())
-        ->setUpdatedAt(new DateTime())
-        ;
-        $manager->persist($object);
-
-        $object = (new User())
-            ->setEmail('admin@user.fr')
-            ->setLastname($faker->lastName)
-            ->setFirstname($faker->firstName)
-            ->setRoles(['ROLE_ADMIN'])
-            ->setPassword($pwd)
+            ->setAddress($faker->address)
+            ->setPhone($faker->phoneNumber)
             ->setIsValidated(true)
             ->setValidationToken("")
             ->setCreatedAt(new DateTime())
             ->setUpdatedAt(new DateTime())
         ;
         $manager->persist($object);
-
-
-        $manager->flush();
     }
 }
