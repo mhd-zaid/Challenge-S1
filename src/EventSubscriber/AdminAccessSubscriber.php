dbd3bd5b7498cc9fef78dca1702b1aa276b74304
jugo92 <jugzek92@outlook.fr>
2023-06-13 14:42:25 +0200

    configure translations to have multi langue

--- src/EventSubscriber/AdminAccessSubscriber.php
+++ src/EventSubscriber/AdminAccessSubscriber.php
@@ -8,22 +8,26 @@ use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class AdminAccessSubscriber implements EventSubscriberInterface
 {
     private $security;
     private $urlGenerator;
 
-    public function __construct(Security $security, UrlGeneratorInterface $urlGenerator)
+    public function __construct(Security $security, UrlGeneratorInterface $urlGenerator, TranslatorInterface $translator)
     {
         $this->security = $security;
         $this->urlGenerator = $urlGenerator;
+        $this->translator = $translator;
     }
 
     public function onKernelRequest(RequestEvent $event)
     {
         $request = $event->getRequest();
         $pathInfo = $request->getPathInfo();
+
+        $this->translator->setLocale('fr');
 
         if ($this->security->isGranted('IS_AUTHENTICATED_FULLY') && !$this->security->getUser()->getIsValidated() && strpos($pathInfo, '/admin') === 0) {
             
