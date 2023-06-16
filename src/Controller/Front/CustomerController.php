e20b495424d1520ea65c7432796aea8e6f4f8908
jugo92 <jugzek92@outlook.fr>
2023-06-16 15:26:26 +0200

    registered user by customer id or without customer id

--- src/Controller/Front/CustomerController.php
+++ src/Controller/Front/CustomerController.php
@@ -17,18 +17,51 @@ class CustomerController extends AbstractController
     #[Route('/new', name: 'app_customer_new', methods: ['GET', 'POST'])]
     public function new(Request $request, CustomerRepository $customerRepository): Response
     {
+        //utilise $request to get ID PARAM
         $customer = new Customer();
         $form = $this->createForm(CustomerType::class, $customer);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            if(isset($_GET['id']) && $_GET['id']){
+            $customer = $customerRepository->find($form->get('id')->getData());
+            $customer->setId($_GET['id']);
+        }else{
+            $id = $this->generateCustomerId($customerRepository);
+            $customer->setId($id);
+        }
+            $customer->setLastname($form->get('lastname')->getData());
+            $customer->setFirstname($form->get('firstname')->getData());
+            $customer->setEmail($form->get('email')->getData());
+            $customer->setPlainPassword($form->get('plainPassword')->getData());
+            $customer->setAddress($form->get('address')->getData());
+            $customer->setPhone($form->get('phone')->getData());
 
             $customerRepository->save($customer, true);
-            return $this->redirectToRoute('front_app_customer_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('front_app_customer_new', [], Response::HTTP_SEE_OTHER);
         }
         return $this->renderForm('front/customer/new.html.twig', [
             'customer' => $customer,
             'form' => $form,
         ]);
     }
+    public function generateCustomerId(CustomerRepository $customerRepository): int
+    {
+        $id = 0;
+        do {
+            $id = random_int(10000, 20000);
+        } while ($this->checkId($id, $customerRepository));
+        return $id;
+    }
+
+    public function checkId(int $id, CustomerRepository $customerRepository): bool
+    {
+        $user = $customerRepository->find($id);
+        if($user === null){
+            return false;
+        }
+
+        return true;
+    }
 }
+
