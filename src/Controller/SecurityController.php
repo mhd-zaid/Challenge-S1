afd06562c376f1fab138b7195229239afee0df5c
Manu1Mackenzii <emmanuelit29@gmail.com>
2023-07-08 00:43:28 +0200

    MAJ

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -29,4 +29,12 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+   
+
+    #[Route(path:'/inscription', name: 'app_inscription')]
+public function inscription(): Response
+{
+    return $this->render('front/customer/register.html.twig');
+}
 }
