d93093bd7d1134b933b6650e210cd255919d549e
jugo92 <jugzek92@outlook.fr>
2023-07-08 19:13:05 +0200

    add validator to check if email user not exist

---
+++ src/Validator/EmailNotExistValidator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use App\Repository\CustomerRepository;
+use App\Repository\UserRepository;
+
+class EmailNotExistValidator extends ConstraintValidator
+{
+    
+    private $customerRepository;
+    private $userRepository;
+
+    public function __construct(CustomerRepository $customerRepository, UserRepository $userRepository)
+    {
+        $this->customerRepository = $customerRepository;
+        $this->userRepository = $userRepository;
+    }
+
+    public function validate($value, Constraint $constraint): void
+    {
+        $email = $this->context->getValue();
+        $user = $this->userRepository->findOneBy([
+            'email' => $email
+        ]);
+        $customer = $this->customerRepository->findOneBy([
+            'email' => $email
+        ]);
+        if($customer === null && $user === null){
+            $this->context->buildViolation($constraint->message)
+            ->setParameter('{{ title }}', $email)
+            ->atPath('id')
+            ->addViolation();
+        } 
+    }
+}
