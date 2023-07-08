d44684c7f2935816c1179785bfe55f712d1007cd
jugo92 <jugzek92@outlook.fr>
2023-07-09 00:51:27 +0200

    add command to update quantity if validity date passed and set status REFUSED to invoice and estimate

--- src/Command/AddQuantityCommand.php
+++ src/Command/AddQuantityCommand.php
@@ -8,6 +8,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Product;
 use App\Entity\Estimate;
+use App\Entity\Invoice;
+use App\Entity\EstimatePrestation;
 
 class AddQuantityCommand extends Command
 {
@@ -24,27 +26,35 @@ class AddQuantityCommand extends Command
     protected function configure()
     {
         $this
-            ->setDescription('Add quantity to a product')
-            ->setHelp('This command adds quantity to a product.');
+            ->setDescription('Add quantity to a product if date is expired')
+            ->setHelp('This command adds quantity to a product and set Status refused.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // Récupérer le produit à mettre à jour (vous pouvez utiliser un identifiant spécifique ou un autre moyen pour récupérer le produit)
-        $productId = 1; // Remplacez 1 par l'ID du produit que vous souhaitez mettre à jour
-        $product = $this->entityManager->getRepository(Product::class)->find($productId);
-        $estimate = $this->entityManager->getRepository(Estimate::class)->find($productId);
 
+        $date = new \DateTime();
+    
+        $estimates = $this->entityManager->getRepository(Estimate::class)->getEstimateByDate($date);
+        foreach($estimates as $estimate){
+            $estimatePrestations = $this->entityManager->getRepository(EstimatePrestation::class)->findBy(['estimate' => $estimate]);
+            foreach($estimatePrestations as $estimatePrestation){
+                $prestation = $estimatePrestation->getPrestation();
+                foreach($prestation->getPrestationProducts() as $prestationProduct){
+                    $productUpdate = $prestationProduct->getProduct();
+                    $productUpdate->setQuantity($prestationProduct->getProduct()->getQuantity() + $prestationProduct->getQuantity());
+                    $this->entityManager->getRepository(Product::class)->save($productUpdate, true);
+                }
+                
+            }
+            $estimate->setStatus('REFUSED');
+            $this->entityManager->getRepository(Estimate::class)->save($estimate, true);
+            $invoice = $estimate->getInvoice();
+            $invoice->setStatus('REFUSED');
+            $this->entityManager->getRepository(Invoice::class)->save($invoice, true);  
+        }
 
-        // Ajouter la quantité souhaitée au produit
-        $quantityToAdd = 1;
-        $product->setQuantity($product->getQuantity() + $quantityToAdd);
-
-        // Enregistrer les modifications en base de données
-        $this->entityManager->flush();
-
-        $output->writeln('Quantity added to the product.');
-
+    
         return Command::SUCCESS;
     }
 }
